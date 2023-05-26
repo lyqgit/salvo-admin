35af42224e1822095153f922b868ef4c710d8443
lyqgit <415285827@qq.com>
2023-05-26 18:04:04 +0800

    去除中间件中不必要的打印

--- src/controller/common_controller.rs
+++ src/controller/common_controller.rs
@@ -4,9 +4,7 @@ use crate::utils::redis;
 
 #[handler]
 pub async fn auth_token(req:&mut Request,res:&mut Response, ctrl: &mut FlowCtrl,depot: &mut Depot){
-  println!("走中间件");
   if let Some(token) = req.headers().get("Authorization"){
-    println!("有token");
     // 验证token
     match redis::get::<i32,&str>(&token.to_str().unwrap().to_string().replace("Bearer ","")){
       Err(_)=>{
@@ -19,7 +17,6 @@ pub async fn auth_token(req:&mut Request,res:&mut Response, ctrl: &mut FlowCtrl,
     }
 
   }else{
-    println!("没有token");
     ctrl.skip_rest();
     res.render(res_json_custom::<()>(401,"token无效".to_string()));
   }
@@ -27,20 +24,20 @@ pub async fn auth_token(req:&mut Request,res:&mut Response, ctrl: &mut FlowCtrl,
 }
 
 #[handler]
-pub async fn catcher_err(&self, _req: &Request, _depot: &Depot, res: &mut Response, ctrl: &mut FlowCtrl) {
+pub async fn catcher_err(res: &mut Response, ctrl: &mut FlowCtrl) {
   println!("发生panic");
   if let Some(status_code) = res.status_code {
     match status_code {
       StatusCode::NOT_FOUND=>{
         // ctrl.skip_rest();
-        println!("任意状态,{:#?}",res.body.size());
+        // println!("任意状态,{:#?}",res.body.size());
         res.render(res_json_custom::<()>(404, "没有对应的接口".to_string()));
       },
       StatusCode::INTERNAL_SERVER_ERROR=>{
         println!("500错误");
         ctrl.skip_rest();
         res.body("".into());
-        res.render(res_json_custom::<()>(500, "发生错误".to_string()));
+        res.render(res_json_custom::<()>(500, "服务器发生错误".to_string()));
       },
       // StatusCode::BAD_REQUEST=>{
       //   println!("BAD_REQUEST");
@@ -49,13 +46,13 @@ pub async fn catcher_err(&self, _req: &Request, _depot: &Depot, res: &mut Respon
       //   // res.render(res_json_custom::<()>(500, "发生错误".to_string()));
       // }
       _=>{
-        println!("任意状态,{:#?}",res.body.size());
+        // println!("任意状态,{:#?}",res.body.size());
         res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
         // res.render(res_json_custom::<()>(500, "发生错误".to_string()));
         res.body("".into());
-        res.render(res_json_custom::<()>(500, "发生错误".to_string()));
+        res.render(res_json_custom::<()>(500, "服务器发生错误".to_string()));
 
-        println!("任意状态,{:#?}",res.body.size());
+        // println!("任意状态,{:#?}",res.body.size());
         ctrl.skip_rest();
         // res.body(res_json_custom::<()>(500, "发生错误".to_string()));
       }
