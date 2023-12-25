f132155bde7d2881e3d0de2a6fd7be13d3828129
lyq <upt3195@dingtalk.com>
2023-12-25 17:09:12 +0800

    feat：rbatis 使用link方法链接，链接失败则不启动后台项目

--- src/utils/mysql.rs
+++ src/utils/mysql.rs
@@ -2,9 +2,8 @@ use crate::GLOBAL_DB;
 
 // 连接数据库
 pub async fn init_db() {
-    GLOBAL_DB.init(
+    GLOBAL_DB.link(
         rbdc_mysql::driver::MysqlDriver {},
         "mysql://root:123456@localhost/ry-vue",
-    )
-        .expect("数据库连接失败");
+    ).await.expect("数据库连接失败");
 }
