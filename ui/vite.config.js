6f66ec9b38ecbd3d16990dc50896d66217c73e3a
peter <huozhiniao@sina.com>
2024-10-14 10:11:01 +0800

    feat: modify notice front end

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig(({ mode, command }) => {
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
     base: VITE_APP_ENV === 'production' ? '/' : '/',
-    plugins: createVitePlugins(env, command === 'build'),
+    plugins: [
+        createVitePlugins(env, command === 'build'),
+    ],
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
